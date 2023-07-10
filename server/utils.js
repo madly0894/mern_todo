82e4ef5d10d6d65a2ff6c266ad39219bff4d6355
Vugar Yusifli <vuqar.yusifli.94@gmail.com>
2023-07-10 04:30:15 +0400

    .

--- server/utils.js
+++ server/utils.js
@@ -1,6 +1,3 @@
-const validate = require('./middlewares/validate.middleware');
-const { body } = require('express-validator');
-
 const getAge = date => new Date().getFullYear() - new Date(date).getFullYear();
 
 module.exports = { getAge };
