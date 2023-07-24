d1a3ba2e75a5ffe626c62122c441de90bca90c0e
Yusifli Vugar <vuqar.yusifli.94@gmail.com>
2023-07-24 19:00:30 +0400

    .

--- client/src/api/user.api.js
+++ client/src/api/user.api.js
@@ -12,7 +12,7 @@ export const getUser = async params => {
    return response.data;
 };
 export const updateUser = async body => $api.put(API_KEYS.UPDATE_USER, body);
-export const uploadUserPicture = async ({ picture }) =>
+export const uploadUserPicture = async picture =>
    $api.patch(API_KEYS.USER_PICTURE, serialize({ picture }, SERIALIZE_OPTIONS));
 export const deleteUser = async () =>
    $api.delete(API_KEYS.DELETE_USER, {
